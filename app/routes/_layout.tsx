2291a54b94d9b0e0345b955a252e219562555da5
irwanphan <irwanphan@gmail.com>
2024-12-25 18:32:32 +0700

    restructure main nav

--- app/routes/_layout.tsx
+++ app/routes/_layout.tsx
@@ -3,30 +3,29 @@ import { IconDatabase, IconPackage } from "@tabler/icons-react";
 
 export default function Layout() {
   return (
-    <div className="flex h-screen">
-      <div className="flex flex-col items-center gap-16 p-6">
-        <header className="flex flex-col items-center gap-9">
-          <h1 className="text-2xl font-bold text-gray-800 dark:text-gray-100">TokoMix</h1>
-        </header>
-        <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700">
-          <ul className="flex flex-col gap-4">
-            <li>
-              <a href="/barang" className="flex items-center gap-2">
-                <IconPackage />
-                Barang
-              </a>
-            </li>
-            <li>
-              <a href="/connection" className="flex items-center gap-2">
-                <IconDatabase />
-                Connection
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      
-      <div className="flex-1 p-6">
+    <div className="flex flex-col  h-screen">
+      <header className="fixed top-0 left-0 right-0 bg-white flex flex-col gap-4 border-b border-gray-200">
+        <div className="flex flex items-center gap-4">
+          <nav className="rounded-xl border p-3">
+            <ul className="flex gap-4">
+              <li>
+                <a href="/barang" className="flex items-center gap-1">
+                  <IconPackage />
+                  Barang
+                </a>
+              </li>
+              <li>
+                <a href="/connection" className="flex items-center gap-1">
+                  <IconDatabase />
+                  Connection
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+
+      <div className="flex-1 flex p-4">
         <Outlet />
       </div>
     </div>
